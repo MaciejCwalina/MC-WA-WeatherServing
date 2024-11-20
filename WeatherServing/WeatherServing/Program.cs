6e33ffa6e8a7c9a7c76309a864af21c6295988a7
MaciejCwalina <150276683+MaciejCwalina@users.noreply.github.com>
2024-11-20 23:17:50 +0100

    Added the GetWeather function
    Added the GetWheather function which allows us to get the Weather based on the cords send by the user.

--- WeatherServing/WeatherServing/Program.cs
+++ WeatherServing/WeatherServing/Program.cs
@@ -1,12 +1,12 @@
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
-    /*.ConfigureServices(services =>
+    .ConfigureServices(services =>
     {
-        services.AddApplicationInsightsTelemetryWorkerService();
-        services.ConfigureFunctionsApplicationInsights();
-    })*/
+        services.AddHttpClient();
+    })
     .Build();
 
 host.Run();
--- WeatherServing/WeatherServing/WeatherServingController.cs
+++ WeatherServing/WeatherServing/WeatherServingController.cs
@@ -8,17 +8,31 @@ namespace WeatherServing
     public class WeatherServingController
     {
         private readonly ILogger<WeatherServingController> _logger;
-
-        public WeatherServingController(ILogger<WeatherServingController> logger)
+        private readonly HttpClient _httpClient;  
+        public WeatherServingController(ILogger<WeatherServingController> logger, IHttpClientFactory httpClientFactory)
         {
             this._logger = logger;
+            this._httpClient = httpClientFactory.CreateClient();
         }
 
-        [Function("WeatherServingController")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        [Function("GetWeather")]
+        public async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+            String? lat = req.Query["lat"];
+            String? lon = req.Query["lon"];
+
+            if (String.IsNullOrEmpty(lat) || String.IsNullOrEmpty(lon)) {
+                return new BadRequestObjectResult("Parameters cannot be empty");
+            }
+
+            HttpResponseMessage resp = await this._httpClient.GetAsync($"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly=temperature_2m&forecast_days=1");
+            if (!resp.IsSuccessStatusCode)
+            {
+                return new BadRequestObjectResult($"Failed to get the weather info response status code is: {resp.StatusCode}\n" +
+                    $"The servers response is {resp.ReasonPhrase}");
+            }
+
+            return new OkObjectResult(await resp.Content.ReadAsStringAsync());
         }
     }
 }
