17a9af02db722fde26d4996941e9be36e3c9208c
MaciejCwalina <150276683+MaciejCwalina@users.noreply.github.com>
2024-11-21 13:06:38 +0100

    Testing new relic integration with .NET

--- WeatherServing/WeatherServing/WeatherServingController.cs
+++ WeatherServing/WeatherServing/WeatherServingController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using NewRelic.Api.Agent;
 
 namespace WeatherServing
 {
@@ -16,8 +17,12 @@ namespace WeatherServing
         }
 
         [Function("GetWeather")]
+        [Transaction]
         public async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
         {
+            NewRelic.Api.Agent.NewRelic.StartAgent();
+            var newRelicAgent = NewRelic.Api.Agent.NewRelic.GetAgent();
+            NewRelic.Api.Agent.NewRelic.SetTransactionName("MC-WA-WeatherServing", "Run");
             req.HttpContext.Response.Headers.Append("Access-Control-Allow-Origin", "*"); // Update with actual origin in production
             req.HttpContext.Response.Headers.Append("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
             req.HttpContext.Response.Headers.Append("Access-Control-Allow-Headers", "Content-Type");
@@ -29,14 +34,22 @@ namespace WeatherServing
                 return new BadRequestObjectResult("Parameters cannot be empty");
             }
 
-            HttpResponseMessage resp = await this._httpClient.GetAsync($"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly=temperature_2m&forecast_days=1");
-            if (!resp.IsSuccessStatusCode)
+            try
             {
-                return new BadRequestObjectResult($"Failed to get the weather info response status code is: {resp.StatusCode}\n" +
-                    $"The servers response is {resp.ReasonPhrase}");
-            }
+                HttpResponseMessage resp = await this._httpClient.GetAsync($"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly=temperature_2m&forecast_days=1");
+                if (!resp.IsSuccessStatusCode)
+                {
+                    return new BadRequestObjectResult($"Failed to get the weather info response status code is: {resp.StatusCode}\n" +
+                        $"The servers response is {resp.ReasonPhrase}");
+                }
 
-            return new OkObjectResult(await resp.Content.ReadAsStringAsync());
+                return new OkObjectResult(await resp.Content.ReadAsStringAsync());
+            }
+            catch (Exception e)
+            {
+                NewRelic.Api.Agent.NewRelic.NoticeError(e);
+                return new BadRequestObjectResult("The api call to the weathe api threw an exception");
+            }
         }
     }
 }
